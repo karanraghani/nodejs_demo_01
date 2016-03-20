c900403f165f808ad823f54260d4b73821ae1ed7
karanraghani <karanraghani14@gmail.com>
2016-03-20 17:55:22 +0530

    select random motivation msg

--- app/index.js
+++ app/index.js
@@ -3,11 +3,13 @@ var app = express();
 
 const motivations = require('motivations');
 
+const pickOne = require('pick-one');
 app.get('/', function(request,response){
 	response.send("May the force be with you!!");
 });
 
+
 app.get('/hello', function(request,response){
-	response.send(motivations[1]);
+	response.send(pickOne(motivations));
 });
 module.exports=app;
