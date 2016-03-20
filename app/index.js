c033ba71d66ed1b18d26cbf3eb73eb4d509b7b90
karanraghani <karanraghani14@gmail.com>
2016-03-20 17:47:54 +0530

    add motivation msgs

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+const motivations = require('motivations');
+
 app.get('/', function(request,response){
 	response.send("May the force be with you!!");
 });
 
 app.get('/hello', function(request,response){
-	response.send("hello");
+	response.send(motivations[1]);
 });
 module.exports=app;
